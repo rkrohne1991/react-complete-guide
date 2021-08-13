b7c7c8ba0258ed12b11579d2ff56c395123342ce
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-08-13 09:59:02 +0100

    Closer look at JSX - comparing previous react version with the newest one, organizing component files, changing functions into arrow functions

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,13 @@
+import React from "react";
+
 import ExpenseItem from "./ExpenseItem";
-import Class from "./Card";
+import Card from "../UI/Card";
 
 import "./Expenses.css";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
-    <Class className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -26,7 +28,7 @@ function Expenses(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </Class>
+    </Card>
   );
 }
 
